12fe4fd676ccb31a4d268b24f933d8925e1ea8d1
Vincent Cannataro <vincent.cannataro@yale.edu>
2017-01-25 19:21:19 -0500

    initial commit

---
+++ fixation_time_functions.R
@@ -0,0 +1,11 @@
+###
+#Simulation of dynamics within the crypt base
+#
+#Cells exist in a ring
+#When a cell divides, the new cell replaces a neighbor of the parent cell
+#
+#Need to record time new cell exists, and whether or not it fixes
+#
+#Run either a certain number of experiments or until a certain number of fixations occur. 
+#
+###

9a9e75cb04e97fa7bef9f670ebad8e8433ed418d
Vincent Cannataro <vincent.cannataro@yale.edu>
2017-02-12 23:57:28 -0500

    added violin plots

--- fixation_time_main.R
+++ fixation_time_main.R
@@ -47,7 +47,7 @@ x16 <- data.matrix$Crypt_size_18
 x17 <- data.matrix$Crypt_size_19
 x18 <- data.matrix$Crypt_size_20
 
-
+#Thanks Ben Bolker for the stackexchange tips
 par(las=1,bty="l")  ## my preferred setting
 ## set up empty plot
 plot(0:1,0:1,type="n",xlim=c(0.5,18.5),ylim=range((c(x1, x2, x3,
@@ -77,3 +77,48 @@ points(mean.vec[3:20],pch=8,col="red",cex=2)
 
 plot(mean.vec[3:20]*mutation.rate,pch=8,col="red",cex=2,ylab="Mean divisions until fixation 
 times mutation rate",xlab="Stem cell compartment population size")
+
+
+
+
+
+
+####Saving plots
+
+png(file="violin_plots_fixation_divisions.png",res=200,units = "in",height = 4,width = 12)
+#Thanks Ben Bolker for the stackexchange tips
+par(las=1,bty="l")  ## my preferred setting
+## set up empty plot
+plot(0:1,0:1,type="n",xlim=c(0.5,18.5),ylim=range((c(x1, x2, x3,
+                                                     x4,
+                                                     x5,
+                                                     x6,
+                                                     x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18))),
+     axes=F,ann=FALSE)
+vioplot(x1, x2, x3,
+        x4,
+        x5,
+        x6,
+        x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,
+        names=paste("",3:20), 
+        col="gold",add=T)
+axis(side=1,at=1:18,labels=paste("",3:20,sep=""))
+axis(side=2,at=seq(0,7000,1000),labels=seq(0,7000,1000))
+title(ylab="Number of divisions until fixation",xlab="Stem cell compartment population size")
+title("Divisions until fixation among 1000 \nfixation events per population size")
+dev.off()
+
+mean.vec <- NULL
+for(i in 1:ncol(data.matrix)){
+  mean.vec[i] <- mean(data.matrix[,i])
+}
+
+points(mean.vec[3:20],pch=8,col="red",cex=2)
+
+png(file="expected_mutations.png",res=200,units = "in",height = 4,width = 8)
+plot(mean.vec*mutation.rate,pch=8,col="red",cex=2,ylab="Mean divisions until fixation times mutation rate",xlab="Stem cell compartment population size")
+dev.off()
+
+
+
+
